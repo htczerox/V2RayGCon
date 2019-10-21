7ae53d32598c702adc29af301ea31c2efcb85694
nobody <nobody@nowhere.mars>
2019-10-21 16:46:55 +0800

    Optimize core events.

--- V2RayGCon/Properties/AssemblyInfo.cs
+++ V2RayGCon/Properties/AssemblyInfo.cs
@@ -33,10 +33,10 @@ using System.Runtime.InteropServices;
 // 可以指定所有值，也可以使用以下所示的 "*" 预置版本号和修订号
 // 方法是按如下所示使用“*”: :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.5.1")]
+[assembly: AssemblyVersion("1.2.5.2")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 
 /*
+ * v1.2.5.2 Optimize core events.
  * v1.2.5.1 (issue #6) Show detail if v2ray-core exit abnormally.
  */
-
